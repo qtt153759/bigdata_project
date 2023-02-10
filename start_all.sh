c3660a0306177fc18e9577c688215191b2a3f46f
qtt153759 <qtt153759@gmail.com>
2023-02-10 07:25:08 +0700

    10/2/2023

--- start_all.sh
+++ start_all.sh
@@ -4,5 +4,5 @@ cd ~/confluent/ &&
 ./bin/kafka-server-start etc/kafka/server.properties &
 cd ~/confluent/ &&
 ./bin/kafka-server-start etc/kafka/server-one.properties &
-cd ~/confluent-7.3.1/ &&
+cd ~/confluent/ &&
 ./bin/schema-registry-start etc/schema-registry/schema-registry.properties &
